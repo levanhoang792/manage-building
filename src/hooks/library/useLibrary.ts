309c0fcf21546eb5d20f7914fca162dd831300fd
HoangLV <hoanglv@stringee.com>
2025-03-06 21:15:03 +0700

    Fix lỗi build

--- src/hooks/library/useLibrary.ts
+++ src/hooks/library/useLibrary.ts
@@ -1,90 +1,90 @@
-import {httpGet, httpPost} from "@/utils/api";
-import {API_ROUTES} from "@/routes/api";
-import {ResRequest} from "@/hooks/model";
-import {
-    ReqCreateLibrary,
-    ReqCreateModelLibrary,
-    ResCreateLibrary,
-    ResCreateModelLibrary,
-    ResLibraries
-} from "@/hooks/library/model";
-import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
-
-const queryKey = "library";
-
-const fetchLibrary = async () => {
-    const resp = await httpGet(
-        {
-            uri: API_ROUTES.LIBRARIES
-        }
-    )
-    return await resp.json() as ResRequest<Array<ResLibraries>>;
-}
-
-const useLibrary = () => {
-    return useQuery({
-        queryKey: [queryKey],
-        queryFn: () => fetchLibrary(),
-    })
-}
-
-/*const fetchLibraryDetail = async (id: string) => {
-    const resp = await httpGet(
-        {
-            uri: API_ROUTES.PRODUCTS_DETAIL.replace(":id", id)
-        }
-    )
-    return await resp.json() as ResRequest<Res3dModelDetailData>;
-}*/
-
-const createLibrary = async (params: ReqCreateLibrary) => {
-    const resp = await httpPost(
-        {
-            uri: API_ROUTES.LIBRARIES,
-            options: {body: JSON.stringify(params)}
-        },
-    )
-    return await resp.json() as ResRequest<ResCreateLibrary>;
-}
-
-const useLibraryCreate = () => {
-    const queryClient = useQueryClient();
-    return useMutation({
-        mutationFn: createLibrary,
-        onSuccess: () => {
-            queryClient.invalidateQueries({queryKey: [queryKey]}).then(r => console.log("Re-fetching data: ", r));
-        },
-        onError: () => {
-        }
-    });
-};
-
-const createModelLibrary = async (params: ReqCreateModelLibrary) => {
-    const resp = await httpPost(
-        {
-            uri: API_ROUTES.LIBRARIES_ADD_MODEL.replace(":id", String(params.library_id)),
-            options: {body: JSON.stringify(params)}
-        },
-    )
-    return await resp.json() as ResRequest<ResCreateModelLibrary>;
-}
-
-const useCreateModelLibrary = () => {
-    const queryClient = useQueryClient();
-    return useMutation({
-        mutationFn: createModelLibrary,
-        onSuccess: () => {
-            queryClient.invalidateQueries({queryKey: [queryKey]}).then(r => console.log("Re-fetching data: ", r));
-        },
-        onError: () => {
-        }
-    });
-};
-
-export {
-    fetchLibrary,
-
-    useLibrary,
-    useLibraryCreate,
-    useCreateModelLibrary
-};
+// import {httpGet, httpPost} from "@/utils/api";
+// import {API_ROUTES} from "@/routes/api";
+// import {ResRequest} from "@/hooks/model";
+// import {
+//     ReqCreateLibrary,
+//     ReqCreateModelLibrary,
+//     ResCreateLibrary,
+//     ResCreateModelLibrary,
+//     ResLibraries
+// } from "@/hooks/library/model";
+// import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
+//
+// const queryKey = "library";
+//
+// const fetchLibrary = async () => {
+//     const resp = await httpGet(
+//         {
+//             uri: API_ROUTES.LIBRARIES
+//         }
+//     )
+//     return await resp.json() as ResRequest<Array<ResLibraries>>;
+// }
+//
+// const useLibrary = () => {
+//     return useQuery({
+//         queryKey: [queryKey],
+//         queryFn: () => fetchLibrary(),
+//     })
+// }
+//
+// /*const fetchLibraryDetail = async (id: string) => {
+//     const resp = await httpGet(
+//         {
+//             uri: API_ROUTES.PRODUCTS_DETAIL.replace(":id", id)
+//         }
+//     )
+//     return await resp.json() as ResRequest<Res3dModelDetailData>;
+// }*/
+//
+// const createLibrary = async (params: ReqCreateLibrary) => {
+//     const resp = await httpPost(
+//         {
+//             uri: API_ROUTES.LIBRARIES,
+//             options: {body: JSON.stringify(params)}
+//         },
+//     )
+//     return await resp.json() as ResRequest<ResCreateLibrary>;
+// }
+//
+// const useLibraryCreate = () => {
+//     const queryClient = useQueryClient();
+//     return useMutation({
+//         mutationFn: createLibrary,
+//         onSuccess: () => {
+//             queryClient.invalidateQueries({queryKey: [queryKey]}).then(r => console.log("Re-fetching data: ", r));
+//         },
+//         onError: () => {
+//         }
+//     });
+// };
+//
+// const createModelLibrary = async (params: ReqCreateModelLibrary) => {
+//     const resp = await httpPost(
+//         {
+//             uri: API_ROUTES.LIBRARIES_ADD_MODEL.replace(":id", String(params.library_id)),
+//             options: {body: JSON.stringify(params)}
+//         },
+//     )
+//     return await resp.json() as ResRequest<ResCreateModelLibrary>;
+// }
+//
+// const useCreateModelLibrary = () => {
+//     const queryClient = useQueryClient();
+//     return useMutation({
+//         mutationFn: createModelLibrary,
+//         onSuccess: () => {
+//             queryClient.invalidateQueries({queryKey: [queryKey]}).then(r => console.log("Re-fetching data: ", r));
+//         },
+//         onError: () => {
+//         }
+//     });
+// };
+//
+// export {
+//     fetchLibrary,
+//
+//     useLibrary,
+//     useLibraryCreate,
+//     useCreateModelLibrary
+// };
--- src/pages/models/components/AdsItem.tsx
+++ src/pages/models/components/AdsItem.tsx
@@ -1,72 +1,72 @@
-import {cn} from "@/lib/utils";
-import {Button} from "@headlessui/react";
-import {PencilSquareIcon} from "@heroicons/react/20/solid";
-import InfoCircle from "@/assets/icons/info-circle.svg?react"; 
-import Icon1 from "@/assets/icons/icon-1.svg?react";
-import {Res3dModelDataData} from "@/hooks/models/model";
-
-interface AdsItemProps {
-    data: Res3dModelDataData;
-}
-
-function AdsItem({data: {thumbnail} }: AdsItemProps) {
-    return (
-        <div className={cn("text-[#222] flex flex-col gap-5")}>
-            <div
-                className={cn(
-                    "relative rounded-2xl bg-[#F8F8F8] h-[300px] flex justify-center items-center"
-                )}
-            >
-                {/* Re-style image */}
-                <img
-                    className={cn("object-contain")}
-                    // fill
-                    src={thumbnail}
-                    alt="Logo page"
-                    sizes="100%"
-                />
-            </div>
-
-            <div className={cn("flex-grow flex flex-col justify-between gap-2.5")}>
-                <div className={cn("flex-grow flex flex-col justify-between")}>
-                    <div className={cn("flex justify-between items-start gap-4")}>
-                        <h2 className={cn("text-xl leading-8 font-semibold line-clamp-2")}>
-                            Lorem ipsum dolor sit
-                        </h2>
-                        <Button>
-                            <PencilSquareIcon className={cn("size-6 mt-1")}/>
-                        </Button>
-                    </div>
-                </div>
-
-                <div className={cn("flex justify-between items-end gap-3")}>
-                    <Button
-                        className={cn(
-                            "rounded-md border border-[#222] py-1 px-2.5 flex justify-center items-center",
-                            "font-bold text-[#222] transition hover:bg-[#7D3200] hover:text-white"
-                        )}
-                    >
-                        Add
-                    </Button>
-
-                    <div className={cn("flex")}>
-                        <Button>
-                            <InfoCircle
-                                width={20}
-                                height={20}
-                            />
-                        </Button>
-                        <Button>
-                            <Icon1
-                                width={20}
-                                height={20}
-                            />
-                        </Button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default AdsItem;
+// import {cn} from "@/lib/utils";
+// import {Button} from "@headlessui/react";
+// import {PencilSquareIcon} from "@heroicons/react/20/solid";
+// import InfoCircle from "@/assets/icons/info-circle.svg?react";
+// import Icon1 from "@/assets/icons/icon-1.svg?react";
+// import {Res3dModelDataData} from "@/hooks/models/model";
+//
+// interface AdsItemProps {
+//     data: Res3dModelDataData;
+// }
+//
+// function AdsItem({data: {thumbnail} }: AdsItemProps) {
+//     return (
+//         <div className={cn("text-[#222] flex flex-col gap-5")}>
+//             <div
+//                 className={cn(
+//                     "relative rounded-2xl bg-[#F8F8F8] h-[300px] flex justify-center items-center"
+//                 )}
+//             >
+//                 {/* Re-style image */}
+//                 <img
+//                     className={cn("object-contain")}
+//                     // fill
+//                     src={thumbnail}
+//                     alt="Logo page"
+//                     sizes="100%"
+//                 />
+//             </div>
+//
+//             <div className={cn("flex-grow flex flex-col justify-between gap-2.5")}>
+//                 <div className={cn("flex-grow flex flex-col justify-between")}>
+//                     <div className={cn("flex justify-between items-start gap-4")}>
+//                         <h2 className={cn("text-xl leading-8 font-semibold line-clamp-2")}>
+//                             Lorem ipsum dolor sit
+//                         </h2>
+//                         <Button>
+//                             <PencilSquareIcon className={cn("size-6 mt-1")}/>
+//                         </Button>
+//                     </div>
+//                 </div>
+//
+//                 <div className={cn("flex justify-between items-end gap-3")}>
+//                     <Button
+//                         className={cn(
+//                             "rounded-md border border-[#222] py-1 px-2.5 flex justify-center items-center",
+//                             "font-bold text-[#222] transition hover:bg-[#7D3200] hover:text-white"
+//                         )}
+//                     >
+//                         Add
+//                     </Button>
+//
+//                     <div className={cn("flex")}>
+//                         <Button>
+//                             <InfoCircle
+//                                 width={20}
+//                                 height={20}
+//                             />
+//                         </Button>
+//                         <Button>
+//                             <Icon1
+//                                 width={20}
+//                                 height={20}
+//                             />
+//                         </Button>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+//
+// export default AdsItem;
